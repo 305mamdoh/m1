b0414b52afa0c9edf670497a1c6ab752460d420b
305mamdoh <107633295+305mamdoh@users.noreply.github.com>
2023-10-11 15:12:33 +0300

    Add files via upload

---
+++ 3/Bein1.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AL-Ali Player</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #fff; /* خلفية الصفحة باللون الأبيض */
+        }
+
+        .logo {
+            width: 500px; /* حجم اللوجو بالعرض المناسب */
+            height: auto; /* للحفاظ على نسب الأبعاد الصحيحة */
+        }
+
+        .video-container {
+            position: relative;
+            width: 90%; /* عرض المشغل يشغل 80% من العرض المتاح */
+            padding-bottom: 56.25%; /* الارتفاع النسبي للفيديو (16:9) */
+            margin-top: 40px; /* تباعد بين المشغل والنص أعلاه */
+            background-color: #958177; /* لون الخلفية للمشغل */
+            border-radius: 30px; /* زوايا مستديرة لحواف الإطار */
+        }
+
+        video {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border-radius: 10px; /* زوايا مستديرة لحواف الفيديو */
+        }
+
+        .title {
+            font-size: 30px;
+            color: #958177; /* لون النص (أصباغ) */
+            background-color: #fff; /* لون خلفية النص باللون الأبيض */
+            margin-top: 40px; /* تباعد بين النص والمشغل */
+            padding: 10px 20px; /* حجم الهامش الداخلي */
+            border: 2px solid #958177; /* حدود بلون النص */
+            border-radius: 5px; /* زوايا مستديرة لحواف النص */
+        }
+
+        .footer {
+            margin-top: 20px; /* تباعد بين النص والتذييل */
+            font-size: 30px; /* زيادة حجم النص في التذييل */
+        }
+
+        .footer a {
+            text-decoration: none;
+            color: #958177; /* لون النص */
+        }
+    </style>
+</head>
+<body>
+    <img class="logo" src="https://k.top4top.io/p_2838q7mhg1.png" alt="شعارك هنا">
+    <div class="video-container">
+        <video controls>
+            <source src="http://176.119.29.7:8080/play/hls/Nm9sUDMyNHFCTUFjUzBrOlh1OEp5MGpoMldwQjQxRjowMDFBNzlBNzhDOUQ6UWtWSlRpQlRVRTlTVkZNZ1VGSkZUVWxWVFNBeElGTkU6MjE=/131f26d9-afb0-480e-8690-e46007de9fc8.m3u8" type="application/x-mpegURL">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+    <div class="title">نتمنى لك مشاهدة ممتعة عزيزي المتابع</div>
+    <div class="footer">
+        <a href="https://instagram.com/alaliplus?igshid=MzRlODBiNWFlZA==">حسابنا على الانستغرام</a>
+    </div>
+</body>
+</html>
